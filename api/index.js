0194ee466f9949f6ace6129bf86306d00dddfa33
Maxx Lehmann <mglehman@mtu.edu>
2018-04-21 10:29:53 +0200

    connect to mongo

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
+const MongoClient = require('mongodb').MongoClient;
 const {
   schema,
   global
 } = require('./src/schema.js');
 const PORT = process.env.PORT || 5000
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/pdb';
+var db
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
@@ -13,5 +16,11 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 
-app.listen(PORT);
-console.log('Running a GraphQL API server at localhost:5000/graphql');
+MongoClient.connect(url, (err, client) => {
+  if (err !== null) {
+    throw new Error(err)
+  }
+  console.log("Connected successfully to DB");
+  db = client.db()
+  app.listen(PORT, () => console.log(`Running a GraphQL API on port ${PORT}`));
+});
